c46ed31cbde41b0dc085b15b398145a670ce6359
bugou <992258645@qq.com>
2013-11-04 22:53:04 +0800

    函数返回地址

---
+++ 函数返回地址/main.cpp
@@ -0,0 +1,24 @@
+//º¯Êý·µ»ØµØÖ·
+
+#include <iostream>
+
+using namespace std;
+
+int func1() {
+    int a = 123456;
+    return a;
+}
+
+int func2()
+    int m;
+    return m;
+}
+
+int main()
+{
+    func1();
+    int a = func2();
+    cout << a << endl;
+    cout << "Hello world!" << endl;
+    return 0;
+}
